7d57437f10ce4c1ecb61af9c8ee04d862192e246
Rubens <rubensprogramcao@gmail.com>
2022-09-18 21:03:53 -0300

    sjango 4.0 upadte

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret! 'django-insecure-u#jmurlhn@l!thc0v(nkfn8hs#zzdknt=&l45s0am280p^241t'
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
